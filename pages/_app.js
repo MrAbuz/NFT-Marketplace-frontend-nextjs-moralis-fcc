6e7570825b278e0bfe5bb4303d75b91c4c131821
MrAbuz <miguelmrosario@gmail.com>
2023-01-28 23:34:43 +0000

    28/01/2023

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css"
+import { MoralisProvider } from "react-moralis"
+import Header from "../components/Header"
 
+//we add the Header here above Component im assuming so that independent of the page we are, the header is always above
+
+//initializeOnMount is false because we're not using the server yet
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <MoralisProvider initializeOnMount={false}>
+            <Header />
+            <Component {...pageProps} />
+        </MoralisProvider>
+    )
 }
